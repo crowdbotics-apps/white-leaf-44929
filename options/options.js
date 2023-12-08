07d59f5b24e97c880c30564f3530fe25b058a44a
Crowdbotics <team@crowdbotics.com>
2023-12-08 21:16:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-leaf-44929",
+  url: "https://white-leaf-44929.botics.co",
+  api: "https://white-leaf-44929.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
